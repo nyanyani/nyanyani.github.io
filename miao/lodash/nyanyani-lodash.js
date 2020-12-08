58e35afb8cf065342d47dbbb0dfe657f1c156e08
nyanyani <dobx@live.com>
2020-12-08 15:01:30 +0800

    lodash-init

---
+++ miao/lodash/nyanyani-lodash.js
@@ -0,0 +1,18 @@
+// var nyanyani = {
+//   concat: function (origin, target) {
+//     let result = []
+//     for (let i = 0; i < ary.length; i++) {
+//       result.push(ary[i])
+//     }
+//     return result
+//   },
+// }
+var nyanayni = function () {
+  function concat(ary) {
+
+  }
+
+  return {
+    concat,
+  }
+}();
